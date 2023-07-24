88129581a5c8c05e1be534afc304422138746705
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2023-07-24 19:36:56 +0300

    fix progression

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -8,18 +8,24 @@ P_NUM_MIN = 10
 P_NUM_MAX = 60
 
 
-def progression_members():
-    random_step = random.randint(MIN_NUMBER, MAX_NUMBER)
-    progression = list(range(P_NUM_MIN, P_NUM_MAX, random_step))
+def progression_members(first_n, last_n, step):
+    progression = []
+    for i in range(first_n, last_n, step):
+        progression.append(i)
     return progression
 
 
-def transformation(progression):
+def transformation(progression_members):
+    step = random.randint(MIN_NUMBER, MAX_NUMBER)
+    first_n = P_NUM_MIN
+    last_n = P_NUM_MAX
+    progression = progression_members(first_n, last_n, step)
     progression_str = ' '.join(map(str, progression))
     random_number = random.choice(progression)
     return progression_str.replace(str(random_number), ".."), random_number
 
 
 def question_and_answer():
-    question, result = transformation(progression_members())
+    question, result = transformation(progression_members)
     return question, str(result)
+
